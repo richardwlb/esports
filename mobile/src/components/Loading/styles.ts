b247306584a8a56c96b234d105ea333975a4d871
Richard W. L. Brehmer <wlb.richard@gmail.com>
2022-09-22 09:41:01 -0300

    feature: server, web and mobile initial changes

---
+++ mobile/src/components/Loading/styles.ts
@@ -0,0 +1,10 @@
+import { Inter_100Thin } from "@expo-google-fonts/inter";
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: Inter_100Thin,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
---
+++ server/build/server.js
@@ -0,0 +1,9 @@
+import express from "express";
+const app = express();
+app.get("/ads", (request, response) => {
+    return response.json({
+        id: 1,
+        name: "Richard",
+    });
+});
+app.listen(3333);
